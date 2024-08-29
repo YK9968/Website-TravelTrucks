e23be516426061eab51bc9e68373c11578863ae3
yurii <robka4an@gmail.com>
2024-08-29 19:45:14 +0300

    fix button load more

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -4,15 +4,29 @@ import { selectItems } from "../../redux/trucks/selectors";
 import CatalogTruckCard from "../../components/CatalogTruckCard/CatalogTruckCard";
 import { useState } from "react";
 import { activateLoader } from "../../redux/trucks/slice";
+import clsx from "clsx";
 
 export default function CatalogList() {
   const trucks = useSelector(selectItems);
   const [visibleCount, setVisibleCount] = useState(4);
+  const [isDisabled, setIsDisabled] = useState(false);
+
   const dispatch = useDispatch();
+
   const loadMore = () => {
+    if (visibleCount >= trucks.length) {
+      setIsDisabled(true);
+      return;
+    }
     dispatch(activateLoader(true));
     setTimeout(() => {
-      setVisibleCount((prevCount) => Math.min(prevCount + 4, trucks.length));
+      setVisibleCount((prevCount) => {
+        const newCount = Math.min(prevCount + 4, trucks.length);
+        if (newCount >= trucks.length) {
+          setIsDisabled(true);
+        }
+        return newCount;
+      });
       dispatch(activateLoader(false));
     }, 500);
   };
@@ -27,7 +41,11 @@ export default function CatalogList() {
         ))}
       </ul>
 
-      <button onClick={loadMore} className={css.loadMore}>
+      <button
+        onClick={loadMore}
+        className={clsx(isDisabled ? css.disLoadMore : css.loadMore)}
+        disabled={isDisabled}
+      >
         Load more
       </button>
     </>
